0b3a961e9bf322bb976def30159d020f96edca43
knakto <kasichonooo@gmail.com>
2025-06-03 15:09:18 +0700

    fixed: case '--' or '++'

--- srcs/builtin/ft_exit.c
+++ srcs/builtin/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: knakto <knakto@student.42bangkok.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 01:41:26 by knakto            #+#    #+#             */
-/*   Updated: 2025/06/02 19:40:58 by knakto           ###   ########.fr       */
+/*   Updated: 2025/06/03 13:05:17 by knakto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,15 @@
 
 static void	error(void)
 {
-	pnf_fd(2, "bash: pwd: too many arguments\n");
+	pnf_fd(2, "bash: exit: too many arguments\n");
 	exit(1);
+}
+
+static bool	conditions(char **cmd, int i)
+{
+	return (!(ft_isalnum(cmd[1][i]) || cmd[1][0] == '-' \
+|| cmd[1][0] == '+') || ft_isalpha(cmd[1][0]) \
+|| cmd[1][1] == '+' || cmd[1][1] == '-');
 }
 
 void	ft_exit(char **cmd)
@@ -31,8 +38,7 @@ void	ft_exit(char **cmd)
 		status = 0;
 		i = -1;
 		while (cmd[1][++i])
-			if (!(ft_isalnum(cmd[1][i]) || cmd[1][0] == '-' \
-|| cmd[1][0] == '+'))
+			if (conditions(cmd, i))
 				status = 1;
 		code = ft_atoi(cmd[1]);
 		if (status)
